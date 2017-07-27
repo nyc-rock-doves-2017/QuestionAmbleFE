049086a7896515b468e573e068298a448bd4c337
Wan Tsui <wanytsui@gmail.com>
2017-07-27 19:19:54 -0400

    Add new questions folder with a questionIndex component.

---
+++ components/questions/questionIndex.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+export default class questionIndex extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
