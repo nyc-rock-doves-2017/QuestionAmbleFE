c2409930c04e37781480aace1b832c281f4d4f3d
Wan Tsui <wanytsui@gmail.com>
2017-07-28 11:36:37 -0400

    Add basic navigation for the questions components.

--- App.js
+++ App.js
@@ -9,27 +9,113 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
 } from 'react-native';
+import { StackNavigator, NavigationActions } from 'react-navigation';
+import QuestionIndex from './components/questions/QuestionIndex.js';
+import QuestionShow from './components/questions/QuestionShow.js';
+import QuestionNew from './components/questions/QuestionNew.js';
+import QuestionEdit from './components/questions/QuestionEdit.js';
 
 export default class QuestionAmbleFE extends Component {
+  static navigationOptions = {
+    title: "QuestionAmbleFE",
+  }
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.welcome}> Directory </Text>
+        <Button onPress={() => this.props.navigation.navigate("QuestionIndex")} title="Question Index"/>
+        <Button onPress={() => this.props.navigation.navigate("QuestionShow")} title="Question Show"/>
+        <Button onPress={() => this.props.navigation.navigate("QuestionNew")} title="Question New"/>
+        <Button onPress={() => this.props.navigation.navigate("QuestionEdit")} title="Question Edit"/>
       </View>
     );
   }
 }
+
+const AppDirectory = StackNavigator({
+  Directory: {
+    screen: QuestionAmbleFE,
+  },
+  // Welcome: {
+  //   screen: Welcome,
+  // },
+  // Login: {
+  //   path:,
+  //   screen: Login,
+  // },
+  // Signup: {
+  //   path:,
+  //   screen: Signup,
+  // },
+  // MainMenu: {
+  //   path:,
+  //   screen: MainMenu,
+  // },
+  // Profile: {
+  //   path:,
+  //   screen: Profile,
+  // },
+  // QuestAction: {
+  //   path:,
+  //   screen: QuestAction,
+  // },
+  // QuestShow: {
+  //   path:,
+  //   screen: QuestShow,
+  // },
+  // QuestCreation: {
+  //   path:,
+  //   screen: QuestCreation,
+  // },
+  // QuestIndex: {
+  //   path:,
+  //   screen: QuestIndex,
+  // },
+  QuestionIndex: {
+    screen: QuestionIndex
+  },
+  QuestionShow: {
+    screen: QuestionShow,
+  },
+  QuestionNew: {
+    screen: QuestionNew,
+  },
+  QuestionEdit: {
+    screen: QuestionEdit,
+  },
+  // ClueShow: {
+  //   path:,
+  //   screen: ClueShow,
+  // },
+  // PlayWindow: {
+  //   path:,
+  //   screen: PlayWindow,
+  // },
+  // ResultNew: {
+  //   path:,
+  //   screen: ResultNew,
+  // },
+  // ResultWin: {
+  //   path:,
+  //   screen: ResultWin,
+  // },
+  // ResultLose: {
+  //   path:,
+  //   screen: ResultLose,
+  // },
+  // RoundShow: {
+  //   path:,
+  //   screen: RoundShow,
+  // },
+  // GameNew: {
+  //   path:,
+  //   screen: GameNew,
+  // },
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -50,4 +136,5 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('QuestionAmbleFE', () => QuestionAmbleFE);
+AppRegistry.registerComponent('QuestionAmbleFE', () => AppDirectory);
+// AppRegistry.registerComponent('QuestionAmbleFE', () => QuestionAmbleFE);
