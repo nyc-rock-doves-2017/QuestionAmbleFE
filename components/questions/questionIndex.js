5df895b1116c696c9758f2846a2eb61105b143dc
Wan Tsui <wanytsui@gmail.com>
2017-07-27 21:29:16 -0400

    Work on question index component.

--- components/questions/questionIndex.js
+++ components/questions/questionIndex.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, SectionList } from 'react-native';
 
-export default class questionIndex extends Component {
+export default class QuestionIndex extends Component {
   render() {
     return (
       <View>
-
+        <Text>Here are your questions:</Text>
+        <Text>List here:</Text>
       </View>
     );
   }
 }
+
+
+// import QuestionIndex from './components/questions/questionIndex.js';
