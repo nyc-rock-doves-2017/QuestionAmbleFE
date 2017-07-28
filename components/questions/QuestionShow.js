86f9dcb43b5517c544caece5b5eda22cff5deb37
Wan Tsui <wanytsui@gmail.com>
2017-07-28 15:16:17 -0400

    Add navigation component to the questions components and forms to the edit and add forms.

--- components/questions/QuestionShow.js
+++ components/questions/QuestionShow.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, SectionList } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, SectionList, Button } from 'react-native';
+import NavigationBar from '../General/NavigationBar.js';
 
 export default class QuestionShow extends Component {
   render() {
     return (
       <View>
+        <NavigationBar/>
+        <Button onPress={() => this.props.navigation.navigate("QuestionEdit")} title="Edit Question"/>
         <Text>The is the question show page:</Text>
       </View>
     );
   }
 }
-
-
-// import QuestionShow from './components/questions/QuestionShow.js';
--- components/questions/questionIndex.js
+++ components/questions/questionIndex.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, SectionList } from 'react-native';
+import NavigationBar from '../General/NavigationBar.js';
 
 export default class QuestionIndex extends Component {
   render() {
     return (
       <View>
+        <NavigationBar/>
         <Text>Here are your questions:</Text>
         <Text>List here:</Text>
       </View>
     );
   }
 }
-
-
-// import QuestionIndex from './components/questions/QuestionIndex.js';
