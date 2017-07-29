e88480a3fb90a53f468342ee42cfe6c7fac47cbd
Wan Tsui <wanytsui@gmail.com>
2017-07-29 16:41:22 -0400

    Add QuestionIndex.js file.

---
+++ components/Questions/QuestionIndex.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, SectionList } from 'react-native';
+import NavigationBar from '../General/NavigationBar.js';
+
+export default class QuestionIndex extends Component {
+  render() {
+    return (
+      <View>
+        <NavigationBar/>
+        <Text>Here are your questions:</Text>
+        <Text>List here:</Text>
+        <Text>Sample Data</Text>
+        <Text>Sample Data</Text>
+        <Text>Sample Data</Text>
+        <Text>Sample Data</Text>
+        <Text>Sample Data</Text>
+      </View>
+    );
+  }
+}
