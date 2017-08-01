29c53e95754046cc2c43a66a28570c9cf9bc691f
Wan Tsui <wanytsui@gmail.com>
2017-08-01 16:18:31 -0400

    Remove debugger statement. Need to add more game logic.

--- components/game/PlayWindow.js
+++ components/game/PlayWindow.js
@@ -4,7 +4,6 @@ import { AppRegistry, StyleSheet, Text, View, SectionList } from 'react-native';
 export default class PlayWindow extends Component {
   render() {
     let question = this.props.screenProps.currentQuestion
-    debugger
     return (
       <View>
         <Text>You found the question! Here is your question:</Text>
